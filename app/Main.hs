b1c1bac8a826d6d7cdff24954aa6ebfc983949a1
Daniel Díaz <diaz_carrete@yahoo.com>
2022-09-18 15:49:24 +0200

    Nested routes.

--- app/Main.hs
+++ app/Main.hs
@@ -19,31 +19,41 @@ import Servant
     Post,
     Proxy (Proxy),
     serve,
-    type (:>),
+    type (:>), Capture,
   )
 import Servant.API.Generic (Generic, GenericMode (type (:-)))
 import Servant.Server.Generic (AsServerT)
 
+type CounterId = Int
+
+data  Counters mode = Counters
+  {
+    counters :: mode :- "counter" :> Capture "counterId" Int :> NamedRoutes Counter
+  }
+  deriving stock Generic
+
 data Counter mode = Counter
-  { increase :: mode :- "counter" :> "increase" :> Post '[JSON] (),
-    query :: mode :- "counter" :> Get '[JSON] Int
+  { increase :: mode :- "increase" :> Post '[JSON] (),
+    query :: mode :- Get '[JSON] Int
   }
   deriving stock (Generic)
 
-type API = NamedRoutes Counter
+type CountersAPI = NamedRoutes Counters
 
-counter :: IORef Int -> Counter (AsServerT Handler)
-counter ref =
-  Counter
-    { increase = liftIO do
-        modifyIORef' ref succ,
-      query = liftIO do
-        readIORef ref
-    }
+counterServer :: IORef Int -> Counters (AsServerT Handler)
+counterServer ref = Counters {
+  counters = \counterId ->
+    Counter
+      { increase = liftIO do
+          modifyIORef' ref succ,
+        query = liftIO do
+          readIORef ref
+      }
+}
 
 main :: IO ()
 main = do
   ref <- newIORef 0
   let app :: Application
-      app = serve (Proxy @API) (counter ref)
+      app = serve (Proxy @CountersAPI) (counterServer ref)
   run 8000 app
