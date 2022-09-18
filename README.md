1ca8e7dd6a43937a93530f11b68a0871c6dd31c9
Daniel Díaz <diaz_carrete@yahoo.com>
2022-09-18 19:58:31 +0200

    Use UUIDs.

---
+++ README.md
@@ -0,0 +1,7 @@
+Playing with Servant named routes
+---------------------------------
+
+- [Servant documentation](https://docs.servant.dev/en/stable/)
+- [servant on Hackage](https://hackage.haskell.org/package/servant)
+- [servant-server on Hackage](https://hackage.haskell.org/package/servant-server)
+- [Named routes in Servant](https://www.tweag.io/blog/2022-02-24-named-routes/)
--- app/Main.hs
+++ app/Main.hs
@@ -2,92 +2,104 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
+import Control.Monad.Error.Class
 import Control.Monad.IO.Class
 import Data.IORef
+import Data.Map.Strict as Map (Map, alterF, empty)
+import Data.Tuple (swap)
+import Data.UUID
+import Data.UUID.V4
 import Network.Wai.Handler.Warp (run)
-import Data.Map.Strict as Map
-import Data.Set as Set
-
 import Servant
   ( Application,
+    Capture,
+    Delete,
     Get,
     Handler,
     JSON,
     NamedRoutes,
     Post,
     Proxy (Proxy),
+    ServerError,
+    err404,
+    err500,
     serve,
-    type (:>), Capture, err404,
+    type (:>),
   )
 import Servant.API.Generic (Generic, GenericMode (type (:-)))
 import Servant.Server.Generic (AsServerT)
-import Control.Monad.Error.Class
-
-type CounterId = Int
-type Counter = Int
-
-data CountersAPI mode = CountersAPI
-  {
-    withCounter :: mode :- Capture "counterId" Int :> NamedRoutes CounterAPI,
-    create :: mode :- Post '[JSON] CounterId
-  }
-  deriving stock Generic
-
-data CounterAPI mode = CounterAPI
-  { increase :: mode :- "increase" :> Post '[JSON] (),
-    query :: mode :- Get '[JSON] Int
-  }
-  deriving stock (Generic)
 
 type API = "counter" :> NamedRoutes CountersAPI
 
-type UseResource r = forall b . (r -> (Maybe r, b)) -> Handler b
+data CountersAPI mode = CountersAPI
+  { counters :: mode :- Capture "counterId" CounterId :> NamedRoutes CounterAPI,
+    create :: mode :- Post '[JSON] CounterId
+  }
+  deriving stock (Generic)
 
-makeCounterAPI :: UseResource Counter -> CounterAPI (AsServerT Handler)
-makeCounterAPI useCounter =
-    CounterAPI
-      { increase = useCounter (\c -> (Just (succ c),())),
-        query = useCounter (\c -> (Just c,c))
-      }
+data CounterAPI mode = CounterAPI
+  { increase :: mode :- "increase" :> Post '[JSON] (),
+    query :: mode :- Get '[JSON] Int,
+    delete :: mode :- Delete '[JSON] ()
+  }
+  deriving stock (Generic)
 
+type CounterId = UUID
+
+type Counter = Int
+
+makeCounterAPI :: WithExistingResource Counter -> CounterAPI (AsServerT Handler)
+makeCounterAPI withExistingResource =
+  CounterAPI
+    { increase = withExistingResource (\c -> (pure (), Just (succ c))),
+      query = withExistingResource (\c -> (pure c, Just c)),
+      delete = withExistingResource (\_ -> (pure (), Nothing))
+    }
 
 makeCountersAPI :: IORef (Map CounterId Int) -> CountersAPI (AsServerT Handler)
-makeCountersAPI ref = CountersAPI {
-  withCounter = \counterId -> do
-    let useCounter :: UseResource Counter
-        useCounter callback = 
-            do r <- liftIO 
-                  do atomicModifyIORef' ref \counterMap ->
-                      case Map.lookup counterId counterMap of
-                        Nothing -> do
-                          (counterMap, Left err404)
-                        Just counterValue -> do
-                          let (c',b) = callback counterValue 
-                          case c' of
-                            Nothing -> 
-                              (Map.delete counterId counterMap, Right b)
-                            Just newCounter ->
-                              (Map.insert counterId newCounter counterMap, Right b)
-               liftEither r
-    makeCounterAPI useCounter,
-  create = do
-      liftIO 
-        do atomicModifyIORef' ref \counterMap ->
-              case Set.lookupMax (Map.keysSet counterMap) of
-                Nothing -> do
-                  let nextCounterId = 0
-                  (Map.insert nextCounterId 0 counterMap, nextCounterId)
-                Just maxCounterId ->  do
-                  let nextCounterId = succ maxCounterId
-                  (Map.insert nextCounterId 0 counterMap, nextCounterId)
-}
+makeCountersAPI ref =
+  CountersAPI
+    { counters = \counterId -> do
+        makeCounterAPI (handleMissing (withResourceInMap ref counterId)),
+      create = do
+        uuid <- liftIO nextRandom
+        withResourceInMap ref uuid \case
+          Nothing -> (Right uuid, Just 0)
+          Just _ -> (Left err500, Nothing) -- UUID collision!
+    }
+
+--
+-- SOME API-GENERIC HELPERS
+-- Not really related to named routes.
+
+type WithResource r = forall b. (Maybe r -> (Either ServerError b, Maybe r)) -> Handler b
+
+type WithExistingResource r = forall b. (r -> (Either ServerError b, Maybe r)) -> Handler b
+
+-- | Handler checking that the resource exists, and throwing 404 if it doesn't.
+handleMissing :: WithResource r -> WithExistingResource r
+handleMissing mightNotExist callback =
+  mightNotExist
+    \mx -> case mx of
+      Nothing -> (Left err404, Nothing)
+      Just x -> callback x
+
+withResourceInMap :: Ord k => IORef (Map k r) -> k -> WithResource r
+withResourceInMap ref k callback =
+  do
+    r <- liftIO do atomicModifyIORef' ref (swap . Map.alterF callback k)
+    liftEither r
+
+--
+--
 
 main :: IO ()
 main = do
