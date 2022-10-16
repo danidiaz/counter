9e52933a85eadccf60e848e54860e05f56453da6
Daniel Díaz <diaz_carrete@yahoo.com>
2022-10-16 12:03:28 +0200

    More docs.

--- lib/Counter/Server.hs
+++ lib/Counter/Server.hs
@@ -17,6 +17,9 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+-- | This module connects the Servant API with the application's model.
+--
+-- In particular, it defines some instances that help map datatypes to and fro. 
 module Counter.Server (
   X(X),
   Env(..),
@@ -47,30 +50,31 @@ import Servant.Server.HandlerContext
 import Servant.Server.ToHandler
     ( FromDTO(..), ToDTO(..), ToServerError(..) )
 
-newtype Env = Env
-  { _handlerContext :: HandlerContext
-  }
-  deriving (Generic)
 
-instance HasHandlerContext Env where
-  handlerContext f s =
-    getField @"_handlerContext" s & f <&> \a -> s {_handlerContext = a}
-
-
+-- | This is a marker type to identify the servant API.
+-- 
+-- In theory, a model could be used to serve different API's, so to avoid
+-- instance collisions we parameterize many helper typeclasses by 'X'.
 data X = X
 
+
+-- | Maps a domain-relevant error to 'ServerError'.
 instance ToServerError X Model.Missing where
   toServerError _ = err404
 
+-- | Maps a domain-relevant error to 'ServerError'.
 instance ToServerError X Model.Collision where
   toServerError _ = err500
 
+-- | DTO mapping.
 instance FromDTO X API.CounterId Model.CounterId where
   fromDTO = coerce
 
+-- | DTO mapping.
 instance ToDTO X API.CounterId Model.CounterId where
   toDTO = coerce
 
+-- | DTO mapping.
 instance ToDTO X API.Counter Model.Counter where
   toDTO Model.Counter {Model.counterId, Model.counterValue} =
     API.Counter
@@ -78,14 +82,30 @@ instance ToDTO X API.Counter Model.Counter where
         API.counterValue = counterValue
       }
 
+-- | DTO mapping.
 instance ToDTO X () () where
   toDTO = id
+
+
+-- | This is the 'ReaderT' environment in which the handlers run.
+newtype Env = Env
+  { _handlerContext :: HandlerContext
+  }
+  deriving (Generic)
+
+instance HasHandlerContext Env where
+  handlerContext f s =
+    -- Artisanal lens.
+    getField @"_handlerContext" s & f <&> \a -> s {_handlerContext = a}
 
 makeServerEnv :: IO Env
 makeServerEnv = do
   pure
     Env
-      { _handlerContext = []
+      { 
+        -- This starts empty, because the handler context is (optially) set by
+        -- "addHandlerContext".
+        _handlerContext = []
       }
 
 --
