7d965e840fd8e73a9a09c1cbd436980b14484f0e
Daniel Díaz <diaz_carrete@yahoo.com>
2022-10-28 21:49:06 +0200

    more patterns

--- lib/Dep/Has/Call.hs
+++ lib/Dep/Has/Call.hs
@@ -2,12 +2,21 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ViewPatterns #-}
-module Dep.Has.Call (pattern Call) where
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UnicodeSyntax #-}
+module Dep.Has.Call (pattern Call, pattern Dep) where
 
 import Dep.Has
 
+-- | 
 pattern Call :: forall env m . (forall r_ x. Has r_ m env => (r_ m -> x) -> x) -> env
 pattern Call call <- (asCall -> call)
 {-# COMPLETE Call #-}
 {-# INLINABLE Call #-}
 
+-- | The @δ@ should be the first argument of the functions we want to invoke.
+pattern Dep :: forall env m . (forall r_ . Has r_ m env => r_ m) -> env
+pattern Dep δ <- ((dep :: env -> forall r_ . Has r_ m env => r_ m) -> δ)
+{-# COMPLETE Dep #-}
+{-# INLINABLE Dep #-}
