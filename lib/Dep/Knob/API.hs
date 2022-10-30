630ce67b57074d7df40a642a09a522135bd9733e
Daniel Díaz <diaz_carrete@yahoo.com>
2022-10-30 19:59:55 +0100

    ...

--- lib/Dep/Knob/API.hs
+++ lib/Dep/Knob/API.hs
@@ -19,9 +19,9 @@ import Servant.API
 import Servant.API.Generic (Generic, GenericMode (type (:-)))
 import Dep.Knob (Knob)
 
-data KnobAPI knob mode = KnobAPI
-  { getKnobConf :: mode :- Get '[JSON] knob,
-    setKnobConf :: mode :- ReqBody '[JSON] knob :> PostNoContent,
+data KnobAPI conf mode = KnobAPI
+  { getKnobConf :: mode :- Get '[JSON] conf,
+    setKnobConf :: mode :- ReqBody '[JSON] conf :> PostNoContent,
     -- | Not sure if DELETE is the semantically correct way of resetting the
     -- Knob's value, but let's go with it for now.
     resetKnob :: mode :- DeleteNoContent
