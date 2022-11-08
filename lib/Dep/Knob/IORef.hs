0ea8b1f7428ee77bb8d7bed903481a7c6a3c741d
Daniel Díaz <diaz_carrete@yahoo.com>
2022-11-08 12:52:07 +0100

    keep alloc simple

--- lib/Dep/Knob/IORef.hs
+++ lib/Dep/Knob/IORef.hs
@@ -4,10 +4,8 @@ import Dep.Knob
 import Data.IORef
 import Control.Monad.IO.Class
 
-allocMake :: (MonadIO n, MonadIO m) => conf -> n (Knob conf m)
-allocMake conf = do 
-    ref <- liftIO $ newIORef conf
-    pure $ make conf ref
+alloc :: MonadIO n => conf -> n (IORef conf)
+alloc conf = liftIO $ newIORef conf
 
 make :: MonadIO m => conf -> IORef conf -> Knob conf m
 make conf ref =
