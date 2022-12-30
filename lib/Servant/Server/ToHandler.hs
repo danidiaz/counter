03aed11758b7ac7cbbc4ca33e81019bc95eb151e
Daniel Díaz <diaz_carrete@yahoo.com>
2022-12-30 21:36:21 +0100

    type synonyms

--- lib/Servant/Server/ToHandler.hs
+++ lib/Servant/Server/ToHandler.hs
@@ -74,14 +74,15 @@ class
   toHandler :: deps -> model -> handler
 
 instance
-  ( Multicurryable (->) modelArgs (ReaderT env IO modelResult) model,
+  ( modelM ~ ReaderT env IO,
+    Multicurryable (->) modelArgs (modelM modelResult) model,
     Multicurryable Either modelErrors modelSuccess modelResult,
-    Multicurryable (->) handlerArgs (ReaderT env Handler handlerSuccess) handler,
+    handlerM ~ ReaderT env Handler,
+    Multicurryable (->) handlerArgs (handlerM handlerSuccess) handler,
     --
-    z ~ ReaderT env IO,
-    AllZip (Convertible mark z deps) handlerArgs modelArgs,
-    All (ToServerError mark z deps) modelErrors,
-    Convertible mark z deps modelSuccess handlerSuccess
+    AllZip (Convertible mark modelM deps) handlerArgs modelArgs,
+    All (ToServerError mark modelM deps) modelErrors,
+    Convertible mark modelM deps modelSuccess handlerSuccess
   ) =>
   ToHandler
     mark
@@ -98,32 +99,32 @@ instance
   where
   toHandler deps model =
     multicurry @(->) @handlerArgs $ \handlerArgs ->
-      let modelArgs :: z (NP I modelArgs)
+      let modelArgs :: modelM (NP I modelArgs)
           modelArgs =
             sequence_NP $
               trans_NP
-                (Proxy @(Convertible mark z deps))
+                (Proxy @(Convertible mark modelM deps))
                 (\(I x) -> convert @mark deps x)
                 handlerArgs
           uncurriedModel = multiuncurry @(->) @modelArgs model
-          modelTip :: z (Either (NS I modelErrors) modelSuccess)
+          modelTip :: modelM (Either (NS I modelErrors) modelSuccess)
           modelTip = do
             args <- modelArgs
             multiuncurry @Either @modelErrors @modelSuccess <$> uncurriedModel args
-          transformErrors :: NS I modelErrors -> z ServerError
+          transformErrors :: NS I modelErrors -> modelM ServerError
           transformErrors errors = do
             mappedErrors :: NS (K ServerError) modelErrors <-
               sequence'_NS $
                 cmap_NS
-                  (Proxy @(ToServerError mark z deps))
+                  (Proxy @(ToServerError mark modelM deps))
                   (\(I e) -> Comp $ K <$> convert @mark deps e)
                   errors
             pure $ collapse_NS mappedErrors
-          transformSuccess :: modelSuccess -> z handlerSuccess
-          transformSuccess = convert @mark @z @deps @modelSuccess @handlerSuccess deps
-          transformMonad :: forall x. ReaderT env IO (Either ServerError x) -> ReaderT env Handler x
+          transformSuccess :: modelSuccess -> modelM handlerSuccess
+          transformSuccess = convert @mark @modelM @deps @modelSuccess @handlerSuccess deps
+          transformMonad :: forall x. modelM (Either ServerError x) -> handlerM x
           transformMonad = coerce
-          handlerTip :: ReaderT env Handler handlerSuccess
+          handlerTip :: handlerM handlerSuccess
           handlerTip = transformMonad do
             tip <- modelTip
             case tip of
@@ -137,49 +138,3 @@ class Convertible mark m deps source target where
 class Convertible mark m deps source ServerError => ToServerError mark m deps source
 
 instance Convertible mark m deps source ServerError => ToServerError mark m deps source
-
--- instance (FromDTO mark a' a, ToHandler mark b b') => ToHandler mark (a -> b) (a' -> b') where
---   toHandler f = toHandler @mark . (f . fromDTO @mark)
---
--- instance ToHandlerResult mark a a' => ToHandler mark (ReaderT env IO a) (ReaderT env Handler a') where
---   toHandler :: ToHandlerResult mark a a' => ReaderT env IO a -> ReaderT env Handler a'
---   toHandler = coerce . fmap (toHandlerResult @mark @a @a')
---
--- class ToHandlerResult mark before after where
---   toHandlerResult :: before -> Either ServerError after
---
--- -- | Extra piece of info to help avoid overlapping instances.
--- data IsResult = IsResult | IsNotResult
---
--- type family DetectResult result :: IsResult where
---   DetectResult (Either e a) = 'IsResult
---   DetectResult _ = 'IsNotResult
---
--- class ToHandlerResult' mark (is :: IsResult) before after where
---   toHandlerResult' :: before -> Either ServerError after
---
--- instance (ToHandlerResult' mark (DetectResult before) before after) => ToHandlerResult mark before after where
---   toHandlerResult = toHandlerResult' @mark @(DetectResult before)
---
--- instance ToDTO mark a' a => ToHandlerResult' mark 'IsNotResult a a' where
---   toHandlerResult' = Right . toDTO @mark
---
--- instance (ToServerError mark err, ToHandlerResult mark a a') => ToHandlerResult' mark 'IsResult (Either err a) a' where
---   toHandlerResult' = \case
---     Left err -> Left (toServerError @mark err)
---     Right r -> toHandlerResult @mark r
---
--- -- | Convert some model error to a 'ServerError'.
--- class ToServerError mark x where
---   toServerError :: x -> ServerError
---
--- -- | Convert a datatype from the model to a DTO.
--- -- Typically used for results.
--- class ToDTO mark dto model | mark dto -> model where
---   toDTO :: model -> dto
---
--- -- | Convert a DTO into a datatype form the model.
--- -- Typically used for request parameters and bodies.
--- class FromDTO mark dto model | mark dto -> model where
---   fromDTO :: dto -> model
---
